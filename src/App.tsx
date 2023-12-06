2e7d19bc822d4bc4ddea0ed1c8de2c25c045c749
Th3Gl0w <skyli-prophecie@live.fr>
2023-12-06 23:35:39 +0900

    pixy3d live demo

---
+++ src/App.tsx
@@ -0,0 +1,141 @@
+import "./index.css";
+import { useState, useEffect, useCallback } from "react";
+import { Session } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import App_ from "./pages/App_";
+import { supabase } from "./database/supabaseClient";
+import { Annotation } from "./types/type";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import Room from "./pages/Room";
+import Home from "./pages/Home";
+import Layout from "./layout/Layout";
+import LoginPage from "./pages/LoginPage";
+import "./pages/LoginPage.css";
+
+//TODO :
+// - pass the session object as child to app component
+// - extract user.id from session object
+// - set the realtime change table
+// - create an annotation with the userId
+// - query all annotations
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+  const [annotationData, setAnnotationData] = useState<Annotation[]>([]);
+  const getAnnotation = useCallback(async () => {
+    try {
+      const { data, error } = await supabase
+        .from("annotation")
+        .select(
+          "id, title, content, position, normal, user_id, room_id, anno_id, username, status"
+        )
+        .order("id", { ascending: true });
+      if (error) {
+        throw error;
+      }
+      if (data) {
+        setAnnotationData(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    getAnnotation();
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, [getAnnotation]);
+
+  return (
+    <Router>
+      <Route exact path="/">
+        <Layout session={session}>
+          <Home />
+        </Layout>
+      </Route>
+      <Route exact path="/create-3d-space">
+        {!session ? (
+          <Layout session={session}>
+            <LoginPage>
+              <Auth
+                supabaseClient={supabase}
+                appearance={{
+                  className: {
+                    button: "login-button",
+                    label: "login-input-fieldname",
+                    input: "login-input",
+                    container: "login-input-container",
+                  },
+                }}
+                providers={[]}
+              />
+            </LoginPage>
+          </Layout>
+        ) : (
+          <Layout session={session}>
+            <Room session={session} />
+          </Layout>
+        )}
+      </Route>
+      <Route exact path="/user/:id">
+        {!session ? (
+          <Layout session={session}>
+            <LoginPage>
+              <Auth
+                supabaseClient={supabase}
+                appearance={{
+                  className: {
+                    button: "login-button",
+                    label: "login-input-fieldname",
+                    input: "login-input",
+                    container: "login-input-container",
+                  },
+                }}
+                providers={[]}
+              />
+            </LoginPage>
+          </Layout>
+        ) : (
+          <Layout session={session}>
+            <UserPage session={session} />
+          </Layout>
+        )}
+      </Route>
+      <Route path="/room/:id">
+        {!session ? (
+          <Layout session={session}>
+            <LoginPage>
+              <Auth
+                supabaseClient={supabase}
+                appearance={{
+                  className: {
+                    button: "login-button",
+                    label: "login-input-fieldname",
+                    input: "login-input",
+                    container: "login-input-container",
+                  },
+                }}
+                providers={[]}
+              />
+            </LoginPage>
+          </Layout>
+        ) : (
+          <App_ session={session} annotationData={annotationData} />
+        )}
+      </Route>
+    </Router>
+  );
+  // return <App_ session={session} annotationData={annotationData} />;
+}
---
+++ src/pages/Room.tsx
@@ -0,0 +1,175 @@
+import Dropzone from "react-dropzone";
+import { useModelStore } from "../store";
+import { useHistory } from "react-router-dom";
+import FolderImage from "../assets/folder.png";
+import { supabase } from "../database/supabaseClient";
+import { Session } from "@supabase/gotrue-js";
+import "./Room.css";
+import createsession_svg from "../assets/createsession.svg";
+import startfromtemplates_svg from "../assets/startfromtemplate.svg";
+import { ChangeEvent, useState } from "react";
+
+type TSession = {
+  session: Session;
+};
+export default function Room({ session }: TSession) {
+  const setRoomName = useModelStore((state) => state.setRoomName);
+  const roomName = useModelStore((state) => state.roomName);
+  const setSaveModel = useModelStore((state) => state.setSaveModel);
+  const setSaveModelNameHooks = useModelStore(
+    (state) => state.setSaveModelNameHooks
+  );
+  const setSaveFileXtenHooks = useModelStore(
+    (state) => state.setSaveFileXtenHooks
+  );
+  const history = useHistory();
+
+  const handleDrop = (acceptedFiles) => {
+    acceptedFiles.map(async (file) => {
+      console.log(file);
+      const urrl = URL.createObjectURL(file);
+      console.log(urrl);
+      setSaveFileXtenHooks(file.name.split(".").pop());
+      setSaveModelNameHooks(file.name.split(".").shift());
+      setSaveModel(urrl);
+      const fileId = `${Math.random()}`;
+      const filePath = `${fileId.split(".").pop()}-${file.name}`;
+      try {
+        const { error: uploadError } = await supabase.storage
+          .from("models")
+          .upload(`public/${filePath}`, file);
+        if (uploadError) {
+          throw uploadError;
+        }
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        const { error } = await supabase.from("room").insert({
+          owner_id: session.user.id,
+          model_id: filePath,
+          room_name: roomName,
+        });
+        if (error) {
+          alert(error.message);
+        }
+      }
+      history.push(`/room/${filePath}`);
+    });
+  };
+  return (
+    <>
+      <div className="create-session-screen">
+        <div className="create-session-container" dir="ltr">
+          <div className="scroll-container-1">
+            <div className="create-session-svg-container">
+              <img
+                src={createsession_svg}
+                style={{ margin: "45px auto 34px auto" }}
+              />
+            </div>
+            <div className="session-name-container">
+              <div className="text-session-name">
+                <div style={{ margin: "auto" }}>Session name</div>
+              </div>
+              <input
+                value={roomName}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  setRoomName(event.target.value)
+                }
+                className="input-session-name"
+                placeholder="Type Session name"
+              />
+            </div>
+
+            <Dropzone onDrop={handleDrop}>
+              {({ getRootProps, getInputProps }) => (
+                <div {...getRootProps({})}>
+                  <input {...getInputProps()} />
+                  <div className="file-upload-container">
+                    <img
+                      style={{ marginTop: "50px", height: "160px" }}
+                      src="https://cdn.discordapp.com/attachments/1180928481366913076/1181012568710578237/fileformat.png?ex=657f82aa&is=656d0daa&hm=81d55668b8e45b611558481b63ef38e91652e491b4618ee048d762640dee8719&"
+                    />
+                    <div>Drag & Drop your 3D model here</div>
+                    <div>
+                      or{" "}
+                      <a
+                        style={{
+                          fontWeight: "700",
+                          textDecoration: "underline !important",
+                          color: "revert",
+                        }}
+                        href="Google.com"
+                      >
+                        Click to Browse Files
+                      </a>
+                    </div>
+                    <div style={{ fontSize: "16px", color: "#505050" }}>
+                      Supported file formats : gltf, glb, obj
+                    </div>
+                  </div>
+                </div>
+              )}
+            </Dropzone>
+
+            <button className="button-create-session">Create session</button>
+            <div className="create-session-scrolldown">
+              <div>
+                <svg
+                  className="svg-scrolldown"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="60"
+                  viewBox="0 0 36 36"
+                >
+                  <path d="M10.5 15l7.5 7.5 7.5-7.5z" />
+                </svg>
+              </div>
+              <img src={startfromtemplates_svg} />
+            </div>
+          </div>
+          <div className="scroll-container-2">
+            <img src={startfromtemplates_svg} style={{ width: "500px" }} />
+            <div className="template-container">
+              <img
+                className="template-item"
+                src={
+                  "https://img.animalplanet.co.kr/news/2020/05/20/700/al43zzl8j3o72bkbux29.jpg"
+                }
+              />
+              <img
+                className="template-item"
+                src={
+                  "https://img.animalplanet.co.kr/news/2020/05/20/700/al43zzl8j3o72bkbux29.jpg"
+                }
+              />
+              <img
+                className="template-item"
+                src={
+                  "https://img.animalplanet.co.kr/news/2020/05/20/700/al43zzl8j3o72bkbux29.jpg"
+                }
+              />
+              <img
+                className="template-item"
+                src={
+                  "https://img.animalplanet.co.kr/news/2020/05/20/700/al43zzl8j3o72bkbux29.jpg"
+                }
+              />{" "}
+              <img
+                className="template-item"
+                src={
+                  "https://img.animalplanet.co.kr/news/2020/05/20/700/al43zzl8j3o72bkbux29.jpg"
+                }
+              />
+              <img
+                className="template-item"
+                src={
+                  "https://img.animalplanet.co.kr/news/2020/05/20/700/al43zzl8j3o72bkbux29.jpg"
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
